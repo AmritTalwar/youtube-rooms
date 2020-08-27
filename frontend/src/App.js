36fb2faecb92222733edf060c7895c613363ceee
Amrit Talwar <amrit-talwar@hotmail.com>
2020-08-27 14:55:12 +0100

    Pass shared socket io connection into Home and Room props

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,13 +5,27 @@ import { Switch, Route, BrowserRouter as Router } from "react-router-dom";
 import Room from "./components/Room";
 import Home from "./components/Home";
 
+import io from "socket.io-client";
+
+const socket = io(process.env.REACT_APP_BACKEND_HOST);
+
 class App extends React.Component {
   render() {
     return (
       <Router>
         <Switch>
-          <Route path="/room/:roomId" component={Room}></Route>
-          <Route path="/home" component={Home}></Route>
+          <Route
+            path="/room/:roomId"
+            component={() => {
+              return <Room socket={socket} />;
+            }}
+          ></Route>
+          <Route
+            path="/home"
+            component={() => {
+              return <Home socket={socket} />;
+            }}
+          ></Route>
         </Switch>
       </Router>
     );
