34a5343a9a034dda323e8977bb918b5148b732ea
Amrit Talwar <amrit-talwar@hotmail.com>
2020-09-03 13:03:52 +0100

    Clicking pause/play to emit to all other sockets in room and change their states

--- frontend/src/components/Room/index.jsx
+++ frontend/src/components/Room/index.jsx
@@ -20,14 +20,48 @@ class Room extends React.Component {
       error: null,
       videoPlaying: false,
     };
+
+    this.handleKeydown = this.handleKeydown.bind(this);
+    this.handlePlayClicked = this.handlePlayClicked.bind(this);
+    this.handlePauseClicked = this.handlePauseClicked.bind(this);
   }
+
+  handleKeydown(event) {
+    const spaceBarClicked = Boolean(event.keyCode === 32);
+    if (spaceBarClicked) {
+      this.state.videoPlaying
+        ? this.handlePauseClicked()
+        : this.handlePlayClicked();
+    }
+  }
+
+  handlePauseClicked() {
+    this.setState({ videoPlaying: false });
+    this.props.socket.emit("pause", this.state.roomId);
+  }
+
+  handlePlayClicked() {
+    this.setState({ videoPlaying: true });
+    this.props.socket.emit("play", this.state.roomId);
+  }
+
   componentDidMount() {
+    document.addEventListener("keydown", this.handleKeydown, false);
+
     this.props.socket.on("roomInfo", (room) => {
       this.setState({ roomLoaded: true, room: room });
     });
 
     this.props.socket.on("err", (message) => {
       this.setState({ error: { message: message } });
+    });
+
+    this.props.socket.on("play", () => {
+      this.setState({ videoPlaying: true });
+    });
+
+    this.props.socket.on("pause", () => {
+      this.setState({ videoPlaying: false });
     });
 
     this.props.socket.emit("joinRoom", this.state.roomId);
@@ -41,11 +75,23 @@ class Room extends React.Component {
         <Youtube videoId="dQw4w9WgXcQ"></Youtube>
 
         {this.state.videoPlaying ? (
-          <Button className={classes.button} startIcon={<Pause />}>
+          <Button
+            className={classes.button}
+            startIcon={<Pause />}
+            onClick={() => {
+              this.handlePauseClicked();
+            }}
+          >
             Pause
           </Button>
         ) : (
-          <Button className={classes.button} startIcon={<PlayArrow />}>
+          <Button
+            className={classes.button}
+            startIcon={<PlayArrow />}
+            onClick={() => {
+              this.handlePlayClicked();
+            }}
+          >
             Play
           </Button>
         )}
