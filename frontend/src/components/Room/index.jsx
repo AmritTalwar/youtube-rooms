6d59e4092c9c2aef1963ebe1351a501a9a32a994
Amrit Talwar <amrit-talwar@hotmail.com>
2020-09-03 11:58:06 +0100

    Create play button in room with no functionality

--- frontend/src/components/Room/index.jsx
+++ frontend/src/components/Room/index.jsx
@@ -1,5 +1,20 @@
 import React from "react";
 import Youtube from "react-youtube";
+import { withStyles } from "@material-ui/core/styles";
+import Button from "@material-ui/core/Button";
+import PlayArrow from "@material-ui/icons/PlayArrow";
+
+const styles = () => ({
+  button: {
+    background: "linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)",
+    border: 0,
+    borderRadius: 3,
+    boxShadow: "0 3px 5px 2px rgba(255, 105, 135, .3)",
+    color: "white",
+    height: 48,
+    padding: "0 30px",
+  },
+});
 
 class Room extends React.Component {
   constructor(props) {
@@ -24,13 +39,17 @@ class Room extends React.Component {
   }
 
   render() {
+    const { classes } = this.props;
     return (
       <div>
         <h1>Room ID: {this.state.roomId}</h1>
         <Youtube videoId="dQw4w9WgXcQ"></Youtube>
+        <Button className={classes.button} startIcon={<PlayArrow />}>
+          Play
+        </Button>
       </div>
     );
   }
 }
 
-export default Room;
+export default withStyles(styles)(Room);
