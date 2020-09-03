ebb43c2d52015a487085ca525a7b79a55d579ea3
Amrit Talwar <amrit-talwar@hotmail.com>
2020-09-03 12:00:26 +0100

    Centralise button styling into globalStyles.js file

--- frontend/src/components/Home/index.jsx
+++ frontend/src/components/Home/index.jsx
@@ -7,17 +7,10 @@ import Grid from "@material-ui/core/Grid";
 import { withStyles } from "@material-ui/core/styles";
 import CircularProgress from "@material-ui/core/CircularProgress";
 import background from "../../images/background.png";
+import { buttonStyle } from "../globalStyles";
 
 const styles = () => ({
-  button: {
-    background: "linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)",
-    border: 0,
-    borderRadius: 3,
-    boxShadow: "0 3px 5px 2px rgba(255, 105, 135, .3)",
-    color: "white",
-    height: 48,
-    padding: "0 30px",
-  },
+  button: buttonStyle,
   background: {
     backgroundImage: `url(${background})`,
     position: "fixed",
